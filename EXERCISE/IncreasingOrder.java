eff6fa51d93408cc97fb7c2b507627beba38ac5f
Samir Poudel <33852346+theoriginalsam@users.noreply.github.com>
2019-02-17 20:53:31 +0545

    Create IncreasingOrder.java

---
+++ EXERCISE/IncreasingOrder.java
@@ -0,0 +1,38 @@
+    import java.util.Scanner;
+    class Main {
+        public static void main(String args[]) {
+            
+            //1
+           int num;
+           boolean flag = false;
+            
+           //2
+           Scanner scanner = new Scanner(System.in);
+            
+           //3
+           System.out.println("Enter a number : ");
+           num = scanner.nextInt();
+            
+           //4
+           int currentDigit = num % 10;
+           num = num/10;
+            
+           //5
+           while(num>0){
+               //6
+               if(currentDigit <= num % 10){
+                   flag = true;
+                   break;
+               }
+               currentDigit = num % 10;
+               num = num/10;
+           }
+            
+           //7
+           if(flag){
+               System.out.println("Digits are not in increasing order.");
+           }else{
+               System.out.println("Digits are in increasing order.");
+           }
+        }
+    }
